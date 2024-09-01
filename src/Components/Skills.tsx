cedfc99a8fd83d16493e476cfeba84956309fae5
mark-elias <mark7elias@gmail.com>
2024-09-01 14:27:28 -0700

    updated Skills section

--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -7,24 +7,31 @@ function Skills() {
           <span className="text-blue-600">Front-End:</span> Javascript,
           Typescript, HTML, CSS
         </li>
+        <li className="ml-5">
+          API's, Responsive Design, Chrome Dev Tools, Validation
+        </li>
         <li className="ml-5">Java, C#, C++, Python</li>
         <li className="ml-5">
-          <span className="text-blue-600">Frameworks:</span> React
+          <span className="text-blue-600">Frameworks/Libraries:</span> React
         </li>
         <li className="ml-5">
-          <span className="text-blue-600">React Ecosystem:</span> Reack Hook
-          Form, Zod, React Query, Axios, React Router, Vite
+          <span className="text-blue-600">React Ecosystem:</span> Vite, Reack
+          Hook Form, Zod, Axios, TanStack Query, React Router
+        </li>
+        <li className="ml-5">
+          <span className="text-blue-600">CSS Frameworks/Libraries:</span>{" "}
+          Tailwind, ChakraUI, Bootstrap, Miligram, DaisyUI
         </li>
         <li className="ml-5">
           <span className="text-blue-600">Version Control:</span> Git, GitHub,
           GitLab
         </li>
         <li className="ml-5">
-          <span className="text-blue-600">CSS Frameworks:</span> Tailwind,
-          ChakraUI, Bootstrap, Miligram
+          <span className="text-blue-600">Deployment & Hosting:</span> GitHub
+          Pages, Hostinger, Vercel
         </li>
         <li className="ml-5">
-          <span className="text-blue-600">Backend:</span> NodeJS, ExpressJS,
+          <span className="text-blue-600">Back-End:</span> NodeJS, ExpressJS,
           Mongoose
         </li>
         <li className="ml-5">
