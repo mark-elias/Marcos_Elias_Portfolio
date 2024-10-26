6a30b562e070c30dce73a3c5c995bb79fa2c41de
mark-elias <mark7elias@gmail.com>
2024-10-26 13:00:07 -0700

    Skills update

--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -4,20 +4,27 @@ function Skills() {
       <h2>Skills</h2>
       <ul className="list-disc">
         <li className="ml-5">
-          <span className="text-blue-600">Front-End:</span> HTML/CSS,
-          JavaScript, TypeScript, React.js
+          <span className="text-blue-600">Front End:</span> React.js,
+          TypeScript, Tailwind CSS
         </li>
         <li className="ml-5">
-          <span className="text-blue-600">Back-End & FullStack:</span> Node.js,
-          Express.js, Next.js
+          <span className="text-blue-600">Full Stack & Back End:</span> Next.js, Node.js,
+          Express.js
         </li>
         <li className="ml-5">
+          <span className="text-blue-600">Forms & Data Validaiton:</span> React-Hook-Form, Zod, Joi
+        </li>
+        {/* <li className="ml-5">
           <span className="text-blue-600">React.js & Next.js Ecosystem:</span>{" "}
           React-Hook-Form, Zod, Axios, TanStack Query, React Router, NextAuth
-        </li>
-        <li className="ml-5">
+        </li> */}
+        {/* <li className="ml-5">
           <span className="text-blue-600">CSS & Styling:</span> Tailwind,
           ChakraUI, Bootstrap, Miligram, DaisyUI
+        </li> */}
+        <li className="ml-5">
+          <span className="text-blue-600">APIs & Data Fetching:</span> REST
+          API's, API Development, TanStack Query, Postman, Axios
         </li>
         <li className="ml-5">
           <span className="text-blue-600">Version Control:</span> Git, GitHub
@@ -30,8 +37,13 @@ function Skills() {
           <span className="text-blue-600">ORMs:</span> Mongoose, Prisma
         </li>
         <li className="ml-5">
-          <span className="text-blue-600">Deployment & Hosting:</span> Vercel,
-          Hostinger, Render, GitHub Pages
+          <span className="text-blue-600">Authentication:</span> Clerk, NextAuth
+        </li>
+        <li className="ml-5">
+          <span className="text-blue-600">Deployment & Hosting:</span> Vercel, Render, Hostinger, GitHub Pages
+        </li>
+        <li className="ml-5 mt-7">
+          <span className="text-blue-600">👍 Still want to learn these:</span> Stripe payments, Supabase, Shadcn UI, React Native, Deno, Bun
         </li>
       </ul>
     </section>
